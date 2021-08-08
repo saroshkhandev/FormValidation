fe764c050e1425592a0e25073a6bbdc431af749e
saroshkhandev <saroshabdullah2013@gmail.com>
2021-08-09 03:35:43 +0530

    Basic form validity for single input / disabled submit button

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -7,6 +7,12 @@ const SimpleInput = (props) => {
   const enteredNameIsValid = enteredName.trim() !== "";
   const nameInputIsInvalid = !enteredNameIsValid && nameInputIsTouched;
 
+  let formIsValid = false;
+
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
+
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
@@ -48,7 +54,7 @@ const SimpleInput = (props) => {
         )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
